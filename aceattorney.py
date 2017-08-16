acc2b6a4d2c263bcc7c3588478d59cf66a852537
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-08-15 17:05:38 -0700

    Updated for rewrite

--- aceattorney.py
+++ aceattorney.py
@@ -17,14 +17,14 @@ class AceAttorney:
     @commands.command(pass_context=True)
     async def holy(self, ctx):
         """Edgeworth's Response (image)"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="https://i.imgur.com/KBWpDpW.png"))
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="https://i.imgur.com/BixOUBV.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="https://i.imgur.com/KBWpDpW.png"))
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="https://i.imgur.com/BixOUBV.png"))
         
     @commands.command(pass_context=True)
     async def holytext(self, ctx):
         """Edgeworth's Response (text)"""
-        await self.bot.delete_message(ctx.message)
+        await ctx.message.delete()
         embed = discord.Embed(title="Miles Edgeworth", color=discord.Color.blue())
         embed.description = "This one single statement is so full of contradictions... For a moment there, I thought I was going to collapse."
         await self.bot.say("", embed=embed)
@@ -32,68 +32,68 @@ class AceAttorney:
     @commands.command(pass_context=True)
     async def press(self, ctx):
         """HOLD IT!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/umt4Pr2.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/umt4Pr2.png"))
         
     @commands.command(pass_context=True)
     async def object(self, ctx):
         """OBJECTION!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/0GTlFg0.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/0GTlFg0.png"))
         
     @commands.command(pass_context=True)
     async def take(self, ctx):
         """TAKE THAT!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/qPIwEaq.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/qPIwEaq.png"))
         
     @commands.command(pass_context=True)
     async def silence(self, ctx):
         """SHUT THE FUCK UP!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/AxbuzZ9.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/AxbuzZ9.png"))
         
     @commands.command(pass_context=True)
     async def gotcha(self, ctx):
         """GOTCHA!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/29ZvDWm.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/29ZvDWm.png"))
         
     @commands.command(pass_context=True)
     async def enough(self, ctx):
         """ENOUGH!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/paNw1dh.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/paNw1dh.png"))
         
     @commands.command(pass_context=True)
     async def fast(self, ctx):
         """NOT SO FAST!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/Nab2PLO.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/Nab2PLO.png"))
         
     @commands.command(pass_context=True)
     async def got(self, ctx):
         """GOT IT!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/J3X6Fbd.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/J3X6Fbd.png"))
         
     @commands.command(pass_context=True)
     async def eureka(self, ctx):
         """EUREKA!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/O8gVn3q.png"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/O8gVn3q.png"))
 
     @commands.command(pass_context=True)
     async def guilty(self, ctx):
         """GUILTY!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/1kq5gau.gif"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/1kq5gau.gif"))
 
     @commands.command(pass_context=True)
     async def notguilty(self, ctx):
         """NOT GUILTY!"""
-        await self.bot.delete_message(ctx.message)
-        await self.bot.send_message(ctx.message.channel, content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/KeggKe4.gif"))
+        await ctx.message.delete()
+        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/KeggKe4.gif"))
 
 def setup(bot):
     bot.add_cog(AceAttorney(bot))
