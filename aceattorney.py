e4620416640919278fbe47e6e29486a9c3608b32
Griffin <4n0nym0ush4ck3r666@gmail.com>
2017-08-26 01:52:02 -0700

    aceattorney cleanup, and hiddenchan fixes

--- aceattorney.py
+++ aceattorney.py
@@ -13,124 +13,164 @@ class AceAttorney:
     async def holy(self, ctx):
         """Edgeworth's Response (image)"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="https://i.imgur.com/KBWpDpW.png"))
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="https://i.imgur.com/BixOUBV.png"))
+        embed = discord.Embed()
+        embed.set_image(url="https://i.imgur.com/KBWpDpW.png")
+        await ctx.send(embed=embed)
+        embed.set_image(url="https://i.imgur.com/BixOUBV.png")
+        await ctx.send(embed=embed)
         
-    @commands.command(pass_context=True)
+    @commands.command(aliases=['ht'], pass_context=True)
     async def holytext(self, ctx):
         """Edgeworth's Response (text)"""
         await ctx.message.delete()
-        embed = discord.Embed(title="Miles Edgeworth", color=discord.Color.blue())
+        embed = discord.Embed()
+        embed.title = "Miles Edgeworth"
         embed.description = "This one single statement is so full of contradictions... For a moment there, I thought I was going to collapse."
-        await ctx.send("", embed=embed)
+        await ctx.send(embed=embed)
 
     @commands.command(pass_context=True)
     async def press(self, ctx):
         """HOLD IT!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/umt4Pr2.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/umt4Pr2.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def object(self, ctx):
         """OBJECTION!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/0GTlFg0.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/0GTlFg0.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def take(self, ctx):
         """TAKE THAT!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/qPIwEaq.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/qPIwEaq.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def silence(self, ctx):
         """SHUT THE FUCK UP!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/AxbuzZ9.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/AxbuzZ9.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def gotcha(self, ctx):
         """GOTCHA!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/29ZvDWm.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/29ZvDWm.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def enough(self, ctx):
         """ENOUGH!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/paNw1dh.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/paNw1dh.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def fast(self, ctx):
         """NOT SO FAST!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/Nab2PLO.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/Nab2PLO.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def got(self, ctx):
         """GOT IT!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/J3X6Fbd.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/J3X6Fbd.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def eureka(self, ctx):
         """EUREKA!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/O8gVn3q.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/O8gVn3q.png")
+        await ctx.send(embed=embed)
 
     @commands.command(pass_context=True)
     async def guilty(self, ctx):
         """GUILTY!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/1kq5gau.gif"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/1kq5gau.gif")
+        await ctx.send(embed=embed)
 
     @commands.command(aliases=['ng'], pass_context=True)
     async def notguilty(self, ctx):
         """NOT GUILTY!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/KeggKe4.gif"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/KeggKe4.gif")
+        await ctx.send(embed=embed)
 
     @commands.command(pass_context=True)
     async def satorha(self, ctx):
         """Satorha!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/FMEzpJ7.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/FMEzpJ7.png")
+        await ctx.send(embed=embed)
 	
     @commands.command(pass_context=True)
     async def hang(self, ctx):
         """Hang on!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/UQUjdnb.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/UQUjdnb.png")
+        await ctx.send(embed=embed)
 	
     @commands.command(pass_context=True)
     async def look(self, ctx):
         """Have a look!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/kcR7d5W.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/kcR7d5W.png")
+        await ctx.send(embed=embed)
     
     @commands.command(pass_context=True)
     async def welcome(self, ctx):
         """Welcome!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/ZDQnh9D.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/ZDQnh9D.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def insolence(self, ctx):
         """Such insolence!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/OIlktYf.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/OIlktYf.png")
+        await ctx.send(embed=embed)
         
     @commands.command(pass_context=True)
     async def shut(self, ctx):
         """Shut up!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://http://i.imgur.com/fk9ewJZ.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://http://i.imgur.com/fk9ewJZ.png")
+        await ctx.send(embed=embed)
 	
     @commands.command(pass_context=True)
     async def over(self, ctx):
         """Overruled!"""
         await ctx.message.delete()
-        await ctx.send( content=None, embed=discord.Embed(color=discord.Color.blue()).set_image(url="http://i.imgur.com/pCLJvzU.png"))
+        embed = discord.Embed()
+        embed.set_image(url="http://i.imgur.com/pCLJvzU.png")
+        await ctx.send(embed=embed)
     
 def setup(bot):
     bot.add_cog(AceAttorney(bot))
--- hiddenchan.py
+++ hiddenchan.py
@@ -13,23 +13,35 @@ class HiddenChan:
         """Show hidden channels"""
         await ctx.message.delete()
         channels = ""
+        nechannels = ""
         hidden = 0
-        total = len(ctx.message.guild.channels)
+        if type(ctx.channel) != discord.channel.DMChannel:
+            total = len(ctx.message.guild.channels)
+        else:
+            await ctx.send(self.bot.bot_prefix + "This command *obviously* doesn't work in a DM, you peasant.")
+            return
         embed = discord.Embed(title="Hidden channels in {}".format(ctx.message.guild))
         for x in ctx.message.guild.channels:
             if not x.permissions_for(ctx.message.author).read_messages:
-                channels += "**# {} **".format(x.name)
+                channels += "**#{}**".format(x.name)
+                nechannels += "#{}".format(x.name)
                 if x.topic == "None" or not x.topic:
                     channels += "\n\n"
+                    nechannels += "\n\n"
                 else:
-                    channels += " - {} \n\n".format(x.topic)
+                    channels += " - {}\n\n".format(x.topic)
+                    nechannels += " - {}\n\n".format(x.topic)
                 hidden += 1
         embed.description = channels
-        embed.set_footer(text="{} out of {} channels are hidden".format(hidden, total))
+        footer = "{} out of {} channels are hidden".format(hidden, total)
+        embed.set_footer(text=footer)
         if not channels:
             await ctx.send(self.bot.bot_prefix + "There are no channels you cannot see!")
         else:
-            await ctx.send(embed=embed)
+            try:
+                await ctx.send(embed=embed)
+            except:
+                await ctx.send("```{}\n\n{}```".format(nechannels, footer))
         
 def setup(bot):
     bot.add_cog(HiddenChan(bot))
