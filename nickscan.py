193440bcbf6e8c83e17ee5b2fef9a5a0f26d2405
GriffinG1 <4n0nym0ush4ck3r666@gmail.com>
2017-10-06 15:49:38 -0700

    2000+ catch

--- nickscan.py
+++ nickscan.py
@@ -13,17 +13,26 @@ class NickScan:
         """Get a list of all your nicknames!"""
         await ctx.message.delete()
         nick = ""
+        bool = True
         for guild in self.bot.guilds:
+            if len(nick + "**Server:** `{}` **Nick:** `{}`\n".format(guild.name, guild.get_member(self.bot.user.id).nick)) > 2000:
+                break
             if guild.get_member(self.bot.user.id).nick:
                 nick += "**Server:** `{}` **Nick:** `{}`\n".format(guild.name, guild.get_member(self.bot.user.id).nick)
         if not nick:
             await ctx.send(self.bot.bot_prefix + "You dont have any nicknames set!")
-        elif len(nick) <= 2000:
-            embed = discord.Embed(title="Servers I Have Nicknames In")
-            embed.description = nick
-            await ctx.send(embed=embed)
         else:
-            await ctx.send("Currently, you have too many nicknames to scan. This will be fixed at a date TBD")
+            if len(nick) <= 1964:
+                nick += "**Could not print the rest, sorry.**"
+                bool = False
+            try:
+                embed = discord.Embed(title="Servers I Have Nicknames In")
+                embed.description = nick
+                await ctx.send(embed=embed)
+            except:
+                await ctx.send("```{}```.format(nick))
+            if bool == True:
+                await ctx.send(self.bot.bot_prefix + "**Could not print the rest, sorry.**")
                 
 def setup(bot):
     bot.add_cog(NickScan(bot))
