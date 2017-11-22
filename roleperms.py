24732963ddff7c52481ad9dbfb95cd421f7d790c
FateWalker <opabravo@gmail.com>
2017-11-22 21:21:33 +0800

    Fix the error when inputting role name with blank
    +Making Embeds Color As Role Color

--- roleperms.py
+++ roleperms.py
@@ -7,19 +7,20 @@ class RolePerms:
 
     def __init__(self, bot):
         self.bot = bot
-        
+
     @commands.command(aliases=['rp'])
-    async def roleperms(self, ctx, msg):
+    async def roleperms(self, ctx, *, msg):
         """Get role perms! The role name must match exactly. Ex for a role "Admin", you'd do >roleperms Admin. If the role name contains a space, the role name must be in quotes."""
         await ctx.message.delete()
         permissions = ""
         permissionsne = ""
+        role=discord.utils.get(ctx.message.guild.roles, name = msg)
         try:
-            for perm in discord.utils.get(ctx.message.guild.roles, name=msg).permissions:
+            for perm in role.permissions:
                 perm = (perm[0].replace("_", " ").title(), perm[1])
                 permissions += "**{}**: {}\n".format(*perm)
                 permissionsne += "{}: {}\n".format(*perm)
-            embed = discord.Embed(title="Permissions for role {}".format(msg))
+            embed = discord.Embed(title="Permissions for role {}".format(msg), color=role.color)
             embed.description = permissions
             try:
                 await ctx.send(embed=embed)
@@ -27,6 +28,6 @@ class RolePerms:
                 await ctx.send("```Permissions for role {}\n\n{}```".format(msg, permissionsne))
         except:
             await ctx.send(self.bot.bot_prefix + "```Couldn't find role, are you sure you typed it correctly?\n\nYou typed: '{}'```".format(msg))
-       
+
 def setup(bot):
     bot.add_cog(RolePerms(bot))
